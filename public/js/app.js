c13e69648a41679ccefbc5b4dfc94f18e221423d
Igor Spasić <igor.spasic@gmail.com>
2020-05-18 22:16:55 +0200

    Enabled providers

--- public/js/app.js
+++ public/js/app.js
@@ -532,9 +532,9 @@ function initFirebase() {
     signInFlow: 'popup',
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
       // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     ],
